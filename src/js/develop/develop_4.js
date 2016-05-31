fa72f516abd220e9a9d01e9d54eb13c15eb176dc
radiks <radion91@gmail.com>
2016-05-31 14:45:32 +0300

    sdfsdf

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,8 +1,19 @@
+function showHeaderMenu(){
+    $('header .navigation li').hover(function() {
+        /* Stuff to do when the mouse enters the element */
+        $(this).addClass('active');
+        $(this).find('>ul').stop().slideDown(500);
 
+    }, function() {
+        /* Stuff to do when the mouse leaves the element */
+        $('header .navigation li').removeClass('active');
+        $('header .navigation li ul').stop().slideUp(200);
+    });
+}
 
 
 $(document).ready(function(){
-
+    showHeaderMenu();
 });
 
 $(window).load(function(){
