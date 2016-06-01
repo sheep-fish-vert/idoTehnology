96d076a6995bad509aa7040f68b9758b4f76949b
Nazar <kryvyynazariy@gmail.com>
2016-06-01 14:52:38 +0300

    big part of sliders

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -6,7 +6,45 @@ $(document).ready(function(){
 		slidesToShow: 3,
   		slidesToScroll: 3,
   		arrows: false,
-  		dots: true
+  		dots: true,
+  		responsive: [
+  			{
+  				breakpoint: 1024,
+  				settings:{
+  					slidesToShow: 2,
+  					slidesToScroll: 2
+  				}
+  			},
+  			{
+  				breakpoint: 900,
+  				settings:{
+  					slidesToShow: 1,
+  					slidesToScroll: 1
+  				}
+  			}
+  		]
+	});
+	$('.home-slider').slick({
+		slidesToShow: 3,
+  		slidesToScroll: 3,
+  		arrows: false,
+  		dots: true,
+  		responsive: [
+  			{
+  				breakpoint: 1280,
+  				settings:{
+  					slidesToShow: 2,
+  					slidesToScroll: 2
+  				}
+  			},
+  			{
+  				breakpoint: 950,
+  				settings:{
+  					slidesToShow: 1,
+  					slidesToScroll: 1
+  				}
+  			}
+  		]
 	});
 });
 
