21c88d3da49c9ad44a5a4e07e2f367730278693e
Nazar <kryvyynazariy@gmail.com>
2016-06-01 08:24:29 +0300

    slaider

--- src/js/develop/develop_6.js
+++ src/js/develop/develop_6.js
@@ -2,7 +2,12 @@
 
 
 $(document).ready(function(){
-
+	$('.biznes-slider').slick({
+		slidesToShow: 3,
+  		slidesToScroll: 3,
+  		arrows: false,
+  		dots: true
+	});
 });
 
 $(window).load(function(){
