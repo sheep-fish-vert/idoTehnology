8a95603dd4b1de379c2072896c799d6cc480d163
Dmitry <dmitry1939@gmail.com>
2016-06-23 14:44:18 +0300

    services nav script for adapt

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,7 +1,51 @@
 
+/* services show-hide left-column nav */
 
+    function showHideServicesLeft(){
+
+        $(document).on('click', function(event){
+
+            if((!$(event.target).is('.column-nav') && !$(event.target).parents('.column-nav').length) || $(event.target).is('li.active.show') || $(event.target).parents('li.active').is('show')){
+                $('.column-nav li.active').removeClass('show');
+                $('.left-column-main li:not(.active)').stop().slideUp(300);
+            }else{
+                $('.column-nav li.active').addClass('show');
+                $('.left-column-main li:not(.active)').stop().slideDown(300);
+            }
+
+        });
+
+        function leftTopAdaptHeight(){
+
+            if($(window).width() < 992){
+
+                var columnActiveHeight = $('.column-nav li.active').outerHeight();
+                $('.column-nav').css({'padding-top':columnActiveHeight+'px'});
+
+            }else{
+
+                $('.column-nav, .column-nav li').removeAttr('style');
+                $('.column-nav li.active').removeClass('show');
+
+            }
+
+        }
+
+        leftTopAdaptHeight();
+
+        $(window).resize(function(){
+
+            leftTopAdaptHeight();
+
+        });
+
+    }
+
+/* /services show-hide left-column nav */
 
 $(document).ready(function(){
+
+    showHideServicesLeft();
 
 });
 
